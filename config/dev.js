915e2db031e12e4b623435d6c202135f5b66c1c6
Alex Sherman <asherman.ca@gmail.com>
2017-12-11 10:39:48 -0800

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '269979045410-j5k7d307cmns93gpacqtargj6s5u50g7.apps.googleusercontent.com',
+  googleClientSecret: 'bPGBMgQ727IyoZOB8fMczQPN',
+  mongoURI: 'mongodb://pokerer:pokerer@ds133166.mlab.com:33166/pokerer',
+  cookieKey: 'ljkvnlj43kbu765sb234vhonbrtuns'
+};
