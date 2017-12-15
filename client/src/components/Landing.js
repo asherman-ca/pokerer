80351042d10d3b55e5adef07446b45aaf2c2d8a7
Alex Sherman <asherman.ca@gmail.com>
2017-12-15 13:08:28 -0800

    loading handling for landing redirect to prevent screen flash

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,30 +7,38 @@ import * as actions from '../actions';
 class Landing extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: true
+    };
   }
 
   componentWillReceiveProps(newProps) {
     if (newProps.location.pathname === '/' && newProps.auth) {
       this.props.history.push('/records');
+    } else {
+      this.setState({ loading: false });
     }
   }
 
   render() {
-    return (
-      <div className="landing">
-        <h1 style={{ marginBottom: '40px' }}>Welcome to Pokerer</h1>
-        <img src="http://classicplayingcards.com/img/cards/hand1.png" />
-        <a href="/auth/google">
-          <RaisedButton
-            label="Login with Google"
-            labelPosition="before"
-            primary={true}
-            icon={<i className="fab fa-google-plus-g" />}
-          />
-        </a>
-      </div>
-    );
+    if (this.state.loading) {
+      return <div />;
+    } else {
+      return (
+        <div className="landing">
+          <h1 style={{ marginBottom: '40px' }}>Welcome to Pokerer</h1>
+          <img src="http://classicplayingcards.com/img/cards/hand1.png" />
+          <a href="/auth/google">
+            <RaisedButton
+              label="Login with Google"
+              labelPosition="before"
+              primary={true}
+              icon={<i className="fab fa-google-plus-g" />}
+            />
+          </a>
+        </div>
+      );
+    }
   }
 }
 
